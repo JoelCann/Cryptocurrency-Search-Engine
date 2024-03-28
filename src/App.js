e8df4fd809c7433bb0c4141b4ac65232498d08a2
JoelCann <joelcann12@gmail.com>
2024-03-28 16:50:04 +0000

    resolved error handling for network

--- src/App.js
+++ src/App.js
@@ -20,11 +20,13 @@ function App() {
   const [euro, setEuro] = useState("")
   const [pound, setPound] = useState("")
   const [desc, setDesc] = useState("")
-  const [ishidden, setIshidden] = useState("true")
+  const [isVerified, setIsVerified] = useState(false)
 
 
   const data_fetch = () => {
 
+
+    console.log(crypto);
 
     const url = 'https://api.coingecko.com/api/v3/coins/' + crypto;
     axios.get(url)
@@ -33,7 +35,7 @@ function App() {
         const resData = result.data
 
         if (crypto === resData.id) {
-          setIshidden(ishidden === false)
+
           setPic(resData.image.large)
           setName(resData.name)
           setSymb(" (" + resData.symbol + ")")
@@ -42,13 +44,26 @@ function App() {
           setEuro("Price in Euros(€): " + resData.market_data.current_price.eur)
           setPound("Price in Pounds(£): " + resData.market_data.current_price.gbp)
           setDesc(JSON.stringify(resData.description.en))
+
+          setIsVerified(!isVerified);
+          preview();
         }
       })
-      .catch(err => { console.log(err); alert(err + `\n \n` + `"` + crypto + `"` + `  is not a valid Cryptocurrency \n Please try again!.`) });
+      .catch(err => {
+        console.log(err);
+        if (err.code === "ERR_BAD_REQUEST") {
+          alert(err.code + `\n \n` + `"` + crypto + `"` + `  is not a valid Cryptocurrency \n Please try again!.`);
+        };
+
+        if (err.code === "ERR_NETWORK") {
+          alert(err.code + `\n \n` + `Please check your network and try again.`);
+        }
+      });
     setCrypto('');
   }
 
 
+  //a function to convert Users search input to lowercase, as Id's from API are lower cased and case sensitive.
   const changeCase = (e) => {
     setCrypto(e.target.value.toLowerCase())
 
@@ -56,8 +71,6 @@ function App() {
 
 
   const markup = { __html: desc };
-
-
 
 
 
@@ -71,41 +84,55 @@ function App() {
 
 
   const preview = () => {
-    console.log(ishidden)
-    if (ishidden === false) {
-      return (
-        <React.Fragment>
-          <div>
-            <div className='containerMargin  text-center p-5 '>
-              <div className='col-md-4 p-5 frostedGlass' /*style={{ backgroundColor: "green" }}*/ >
-                <img src={pic} width="150" className="py-3 " alt="" /><br />
-                <h1 >{name}{symb}</h1><br />
-                <br />
-                <p style={{ Color: "green[500]" }}>
-                  {dollar}<br />
-                  {euro}<br />
-                  {pound}
-                </p><br />
-                <p><a href={lynk}>Official Website</a></p>
-              </div>
-            </div>
-
-
-            <div className='row containerMargin container-fluid' >
-              <div className='col-md-8 p-5 text-center py-10 justify-content-center frostedGlass2' >
-                <div style={{ fontWeight: "120px", fontSize: "20px" }} dangerouslySetInnerHTML={markup} />
-              </div>
+    // console.log(ishidden + " (from preview function)")
+    // if (ishidden === false) {
+    return (
+      <React.Fragment>
+        <div>
+          <div className='containerMargin  text-center p-5 '>
+            <div className='col-md-4 p-5 frostedGlass' >
+              <img src={pic} width="150" className="py-3 " alt="" /><br />
+              <h1 >{name}{symb}</h1><br />
+              <br />
+              <p style={{ Color: "green[500]" }}>
+                {dollar}<br />
+                {euro}<br />
+                {pound}
+              </p><br />
+              <p><a href={lynk}>Official Website</a></p>
             </div>
           </div>
 
-        </React.Fragment>
 
-      );
-      // setIshidden(ishidden === true);
-    }
+          <div className='row containerMargin container-fluid' >
+            <div className='col-md-8 p-5 text-center py-10 justify-content-center frostedGlass2' >
+              <div style={{ fontWeight: "120px", fontSize: "20px" }} dangerouslySetInnerHTML={markup} />
+            </div>
+          </div>
+        </div>
 
+
+      </React.Fragment>
+    );
+
+
+    //}
+
+    // else {
+    //   return null;
+    // }
   };
 
+
+
+  const displayPreview = () => {
+    setIsVerified(!isVerified);
+  }
+
+  const retNull = () => {
+    setIsVerified(!isVerified);
+    return null;
+  }
 
   useOnKeyPress(data_fetch, "Enter");
 
@@ -113,7 +140,7 @@ function App() {
   return (
     <React.Fragment>
       <body>
-        <div className='App' style={{ backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', backgroundAttachment: 'fixed', backgroundSize: "cover", width: "100vw", height: "200vh" }}>
+        <div className='App' style={{ backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', backgroundAttachment: 'fixed', backgroundSize: "cover", width: "100vw", height: "210vh" }}>
           <header className="p-5" >
             <div className="App p-4  container-fluid" >
               <h1 className="title pt-5">Cryptocurrency Search Engine</h1>
@@ -123,13 +150,32 @@ function App() {
             </div>
 
             <div>
-              <div className="col-md-12 d-flex justify-content-center p-5 " >
-                <input type="search" value={crypto} onChange={(e) => setCrypto(e.target.value)} onBlur={changeCase} placeholder="Input cryptocurrency" className="w-25 form-control " required />
-                <button type="submit" onClick={() => { data_fetch(); }} className='btn btn-secondary '>Search</button >
+              <div className="col-md-12 d-flex justify-content-center p-5 "  >
+                <input
+                  type="search"
+                  value={crypto}
+                  onChange={(e) => setCrypto(e.target.value)}
+                  onBlur={changeCase}
+                  placeholder="Input cryptocurrency"
+                  className="w-25 form-control "
+                  required
+                />
+                <button
+                  type="submit"
+                  onClick={() => {
+                    data_fetch();
+                    displayPreview();
+                  }
+                  }
+                  className='btn btn-secondary '
+                >
+                  Search
+                </button >
               </div>
             </div>
-            <div >
-              {preview()}
+            <div className="container-fluid">
+              {isVerified ? preview() : null}
+              {/* {preview()} */}
             </div>
 
 
